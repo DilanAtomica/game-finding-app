cb3d59cc162cf2d8fb1a3b882becd73fc918470a
DilanAtomica <dilan-shwane@hotmail.com>
2022-05-24 22:33:01 +0200

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import FrontPage from "./Pages/FrontPage";
 
 
 function App() {
   return (
     <div className="App">
 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<> <FrontPage /> </>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
