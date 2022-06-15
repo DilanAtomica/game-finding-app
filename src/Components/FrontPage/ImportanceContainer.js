7dc9cc0248084a9c966b75d1a4e9e188f5ede5db
DilanAtomica <dilan-shwane@hotmail.com>
2022-06-15 14:26:22 +0200

    Imporved responsivness for mobile

--- src/Components/FrontPage/ImportanceContainer.js
+++ src/Components/FrontPage/ImportanceContainer.js
@@ -10,7 +10,7 @@ function ImportanceContainer({handlePreferences}) {
 
     return (
         <section className="importanceContainer" id="phaseFour">
-            <h1>Are any of these important?</h1>
+            <h1>Choose preferences</h1>
             <ul className="importanceOptions">
                 <ImportanceButton handlePreferences={handlePreferences} idName="great-soundtrack" color="#00ABBD" importanceName="Great Soundtrack" />
                 <ImportanceButton handlePreferences={handlePreferences} idName="atmospheric" color="#0099DD" importanceName="Atmospheric" />
